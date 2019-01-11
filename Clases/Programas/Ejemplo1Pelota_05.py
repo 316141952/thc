995149fd9a1acf180421c45f39c87d6f999dafa0
316141952 <vocado137@gmail.com>
2019-01-11 14:24:38 -0600

    Agregamos la versión final del ejemplo de la pelota y vimos ejemplos de cómo se muestra una variable en python y agregamos comentarios

---
+++ Clases/Programas/Ejemplo1Pelota_05.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+V0 = 34
+g = 9.81
+t = 4.3
+y = V0 * t - 1.0/2 * g * t**2
+print 'La posición de la pelota en el t = %g es %.4f\n%f' % (t,y,t)
---
+++ Clases/Programas/EjemplosPrint.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+i = 62
+r = 189876545.7654675432
+
+# Print out numbers with quotes "" such that we see the
+# width of the field
+print '"%d"' % i       # minimum field
+#l a parte entera del número
+print '"%5d"' % i      # field of width 5 characters
+# usar espacio para 5 cifras 
+print '"%05d"' % i     # pad with zeros
+# acomapñar la cifra con 5 ceros a la izquierda
+print '"%g"' % r       # r is big number so this is scientific notation
+# como r es un número muy grande se expresa en notación científica
+print '"%G"' % r       # E in the exponent
+# se utiliza E en el exponente
+print '"%e"' % r       # compact scientific notation
+# compactar notación científica
+print '"%E"' % r       # compact scientific notation
+# compactar notación científica
+print '"%20.2E"' % r   # 2 decimals, field of width 20
+# usar espacio para 20 cifras y usar 2 decimales
+print '"%30g"' % r     # field of width 30 (right-adjusted)
+# usar el espacio para 3 cifras 
+print '"%-30g"' % r    # left-adjust number
+# ajustar el número a la izquierda
+print '"%-30.4g"' % r  # 3 decimals
+# ajustar el número a la izquierda con 3 decimales 
+print '%s' % i   # can convert i to string automatically
+# convierte i en una cadena automáticamente 
+print '%s' % r
+# convierte r en una cadena automáticamente
+
+# Use %% to print the percentage sign
+# usa %% para mostrar el signo de porcentaje  
+print '%g %% of %.2f Euro is %.2f Euro' % \
+      (5.1, 346, 5.1/100*346)
