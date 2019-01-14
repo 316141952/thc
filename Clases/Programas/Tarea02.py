55240ad66a5b5cbcef34aaa695de823d2aba69cb
316141952 <vocado137@gmail.com>
2019-01-14 14:11:24 -0600

    Se agregaron las 5 bitácoras de la primera semana y el cuestionario

--- Clases/Programas/Tarea02.py
+++ Clases/Programas/Tarea02.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 t = 20
-x1 = 15 * t #camión
-x2 = 1.0/2 * 1.5 *t**2 #automóvil
-print 'La posición en que se encuentran en el t = %g es x1 = %d para el camión y x2 = %d para el automóvil' % (t,x1)
+v = 15
+a = 1.5
+x1 = v * t #camión
+x2 = 1.0/2 * a *t**2 #automóvil
+print 'La posición en que se encuentran en el t = %g es x1 = %d para el camión y x2 = %d para el automóvil' % (t,x1,x2)
 
-def pos(t):
-    x1 = 15 * t
-    x2 = 1.0/2 * 1.5 * t**2
+def pos(t,a,v):
+    x1 = v * t
+    x2 = 1.0/2 * a * t**2
     return(x1,x2)
-
-
-
-
-
 
