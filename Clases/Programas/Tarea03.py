f2ebb5bae1243d45d39e4b35b26a155fa2100b6a
316141952 <vocado137@gmail.com>
2019-01-15 14:04:04 -0600

    Pasé a Latex la bitácora 6 y la Tarea 3 la pasé al shell de python, e hicimos un ejercicio con la funcion raiz que creamos

--- Clases/Programas/Tarea03.py
+++ Clases/Programas/Tarea03.py
@@ -1,4 +1,9 @@
-def diana (x,y) :
+def punto (x,y) :
+    import math
+    d = math.sqrt ( (x-15)**2 + (y-20)**2 )
+    return d
+    
+def diana1 (x,y) :
     if y > 30 and x < 5 :
         return 3
     if y >= 10 and y <= 30 and x < 5 :
@@ -7,8 +12,6 @@ def diana (x,y) :
         return 3
     if y > 30 and x >= 5 and x <=25 :
         return 7
-    if y >= 10 and y <= 30 and x >= 5 and x <= 25 :
-        return 10
     if y < 10 and x >= 5 and x <= 25 :
         return 7
     if y > 30 and x > 25 :
@@ -17,5 +20,8 @@ def diana (x,y) :
         return 5
     if y < 10 and x > 25 :
         return 3
-
-
+    if punto (x,y) < 10 :
+        return 10
+    else :
+        return 100
+        
