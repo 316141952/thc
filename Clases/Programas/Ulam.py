49923e12b77ff1ec716ac2f2e5d744a03cfb2552
316141952 <vocado137@gmail.com>
2019-01-15 17:07:51 -0600

    Hicimos otro ejercicio en Latex en el que vimos cómo escribir  expresiones matemáticas

---
+++ Clases/Programas/Ulam.py
@@ -0,0 +1,22 @@
+def ulam (x) :
+    #if (x/2)*2-x == 0 :
+    if x%2 == 0 :
+        return x/2
+    else :
+        return 3*x + 1
+def suc (x) :
+    i = 0
+    #while x > 1 :S
+    while x != 1 :
+        x = ulam (x)
+        i = i + 1 
+    return i
+        
+
+print suc (7)
+print suc (26)
+print suc (1024)
+print suc (72)
+print suc (1524927)
+print suc (2)
+
--- Clases/Programas/funcion01.py
+++ Clases/Programas/funcion01.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def vAbsoluto (x) :
     if x >= 0 :
         return (x)
@@ -8,15 +9,18 @@ def raiz (x) :
     h = x
     b = 1.0
     e = 0.0001
+    i = 0 #cuenta el número de veces que se ejecuta el ciclo
     while vAbsoluto (b-h) > e :
         h = ( b+h )/2
         b = x/h
+        i = i + 1
+    print "EL ciclo se repitió %d veces" %(i)
     return (b)
 
 def raiz1 (x) :
     h = x
     b = 1.0
-    e = 0.0001
+    e = 0.0001 #representa el error, decimales de precisión
     while vAbsoluto (b-h) > e :
         h = ( b+h )/2
         b = x/h
@@ -30,4 +34,3 @@ print raiz (9)
 print raiz (9.1)
 print raiz (1000000)
 print raiz1 (4)
-
