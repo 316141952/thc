698f897bb76c447cf19b9697bbdb1d7d361e48b9
316141952 <vocado137@gmail.com>
2019-01-25 15:17:03 -0600

    Ya acabé el problema 2

---
+++ Clases/Programas/Practica/Problema2S.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+'''
+Valeria Ortiz Cervantes, 316141952
+Taller de Herramientas Computacionales
+Aquí puse la parte interactiva
+'''
+
+import Problema2
+from Problema2 import comp 
+
+n = input ('''¿A partir de qué número quieres calcular
+la suma de cuadrados? ''')
+print comp (n)
