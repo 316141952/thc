878d386f7cf5eef65b252f0b940385f12a5be423
316141952 <valeria.ccervantes@ciencias.unam.mx>
2019-01-26 23:31:09 -0600

    Problema 3 de Python fácil

---
+++ Clases/Programas/PythonFacil/Problema3.py
@@ -0,0 +1,25 @@
+def prim_o_comp (x) :
+    while x > 0 :
+        if x == 2 or x == 3 or x == 5 or x == 7 :
+            return 'Primo'
+        if x%2 == 0 :
+            return 'Compuesto'
+        if x%3 == 0 :
+            return 'Compuesto'
+        if x%4 == 0 :
+            return 'Compuesto'
+        if x%5 == 0 :
+            return 'Compuesto'
+        if x%6 == 0 :
+            return 'Compuesto'
+        if x%7 == 0 :
+            return 'Compuesto'
+        if x%8 == 0 :
+            return 'Compuesto'
+        if x%9 == 0 :
+            return 'Compuesto'
+        if x%10 == 0 :
+            return 'Compuesto'
+        else :
+            return 'Primo'
+    
---
+++ Clases/Programas/PythonFacil/Problema3S.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+'''
+Valeria Ortiz Cervantes, 316141952
+Taller de Herramientas Computacionales
+Aquí probé algunos ejemplos para comprobar la función,
+y después puse una parte interactiva para el bash.
+Bueno, convenientemente justo había resuelto este mismo problema desde
+la tarea 4.
+'''
+import Problema8
+from Problema8 import prim_o_comp as poc
+x = input ("¿Cuál número natural quieres saber si es primo o compuesto?")
+print 'El %d es %s' % (x,poc(x))
