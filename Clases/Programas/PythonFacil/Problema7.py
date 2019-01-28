95a088b3f2cb867e4a3b5564b4957fb09a6b5ebd
316141952 <valeria.ccervantes@ciencias.unam.mx>
2019-01-27 20:06:37 -0600

    El problema 7 de python fácil

---
+++ Clases/Programas/PythonFacil/Problema7.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+def f (x) :
+    a = 0
+    b = 0
+    for i in x :
+        if i > 0 :
+            a += i
+        else :
+            a -= i
+    return a + b
+
+'''El problema consiste en determinar el
+propósito de la función f y darle un buen
+nombre.
+El propósito de la función f es dar la
+suma de los elementos de una lista.
+Yo creo que un buen nombre sería suma_lista.
+Como no definí ninguna función yo
+no creo que necesite un archivo script.
+'''
